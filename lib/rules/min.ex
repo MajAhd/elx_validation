faed8ee992e11ec656df33db37d3aa3e13a26b46
Majid Ahmadi <mjd.ahd.tbr@gmail.com>
2021-06-05 20:45:52 +0430

    validation min added

--- lib/rules/min.ex
+++ lib/rules/min.ex
@@ -7,7 +7,11 @@ defmodule ElxValidation.Min do
      max : 3 / value 4 -> passed
      max : 3 / value "hello" -> passed (hi is 5 char > 3)
   """
-  def is_minimum(min, value) do
-    true
+  def is_minimum(target, value) do
+    cond do
+      is_bitstring(target) -> String.length(target) >= String.to_integer(value)
+      is_integer(target) -> target >= String.to_integer(value)
+      true -> false
+    end
   end
 end
--- test/elx_validation_test.exs
+++ test/elx_validation_test.exs
@@ -416,5 +416,46 @@ defmodule ElxValidationTest do
              failed: true
            }
   end
+  # test Min
+  test "min passed" do
+    data = %{
+      name: "john doe",
+      age: 20
+    }
+    rules = [
+      %{
+        field: "name",
+        validate: ["min:5"]
+      },
+      %{
+        field: "age",
+        validate: ["min:20"]
+      }
+    ]
+    assert ElxValidation.make(data, rules) == %{errors: [], failed: false}
 
+  end
+  test "min failed" do
+    data = %{
+      name: "john",
+      age: 19
+    }
+    rules = [
+      %{
+        field: "name",
+        validate: ["min:5"]
+      },
+      %{
+        field: "age",
+        validate: ["min:20"]
+      }
+    ]
+    assert ElxValidation.make(data, rules) == %{
+             errors: [
+               name: ["The name must be at least 5."],
+               age: ["The age must be at least 20."]
+             ],
+             failed: true
+           }
+  end
 end
