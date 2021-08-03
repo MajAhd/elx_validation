ac901cdc30c21e6e856df12bd3d83add35bcaae6
Majid Ahmadi <mjd.ahd.tbr@gmail.com>
2021-08-03 15:53:26 +0430

    lib image added

---
+++ lib/rules/file.ex
@@ -0,0 +1,22 @@
+defmodule ElxValidation.File do
+  @moduledoc """
+
+  """
+  def is_file(target) do
+    true
+  end
+
+  def mimes(target) do
+
+  end
+  def mime_types(target) do
+    true
+  end
+
+  def max_size(target) do
+
+  end
+  def min_size(target) do
+    true
+  end
+end
